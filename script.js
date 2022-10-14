396cfcbc750798207ca911cc15b7f099222c1ac6
Daniel Hjartland <squashical@gmail.com>
2022-10-14 10:26:31 +0200

    Fix logic to add listener upon grid creation and add change size button

--- script.js
+++ script.js
@@ -1,34 +1,68 @@
-const cellSize = '40px'
-const gridSize = 16
+// const cellSize = '40px'
+// width: ${cellSize}; height: ${cellSize}; 
+const gridContainer = document.getElementById('grid-container');
+let gridSize = 16;
 
-const container = document.getElementById('container')
-container.setAttribute('style', 'display: flex; flex-direction: column;')
+function createGrid(gridSizeVar) {
 
-function createGrid() {
-  for (var i = 0; i < gridSize; i++) {
+  resetGrid(gridContainer);
+
+  for (var i = 0; i < gridSizeVar; i++) {
     var row = document.createElement('div');
-    row.setAttribute('style', 'display:flex');
+    row.setAttribute('style', 'display:flex; flex: auto');
     
-    for (var j = 0; j < gridSize; j ++) {
+    for (var j = 0; j < gridSizeVar; j++) {
       var cell = document.createElement('div');
-      cell.setAttribute('style', `width: ${cellSize}; height: ${cellSize}; border: solid black 1px; background-color: blue`);
+      cell.setAttribute('style', `flex: auto; border: solid black 1px; background-color: blue`);
       cell.classList.add('cell')
 
       row.appendChild(cell);
     }
-    container.appendChild(row);
+    gridContainer.appendChild(row);
+  }
+  addListenersToGrid()
+}
+
+function resetGrid(gridContainer) {
+  var child = gridContainer.lastElementChild;
+  while (child) {
+    gridContainer.removeChild(child);
+    child = gridContainer.lastElementChild;
   }
 }
 
-createGrid();
+function addListenersToGrid() {
+  const gridCells = document.querySelectorAll('.cell');
 
-const gridCells = document.querySelectorAll('.cell');
+  gridCells.forEach((cell) => {
+    cell.addEventListener('mouseover', () => {
+      cell.style.backgroundColor = 'red';
+    });
+    cell.addEventListener('mouseout', () => {
+      cell.style.backgroundColor = 'blue';
+    });
+  });
+}
 
-gridCells.forEach((cell) => {
-  cell.addEventListener('mouseover', () => {
-    cell.style.backgroundColor = 'red';
-  })
-  cell.addEventListener('mouseout', () => {
-    cell.style.backgroundColor = 'blue';
-  })
-});
+function getNewGridSize() {
+  gridSize = parseInt(prompt('Please choose a number between 1 and 100:'))
+  while (gridSize < 1 || gridSize > 100){
+    gridSize = parseInt(prompt('Please choose a number between 1 and 100:'))
+    }
+  return gridSize;
+}
+
+
+function createNewGrid() {
+  var newGridSize = getNewGridSize();
+  console.log(newGridSize)
+  if (isNaN(newGridSize)) {
+    newGridSize = 16;
+  }
+  createGrid(newGridSize);
+}
+
+const changeSizeBtn = document.getElementById('changeSizeBtn')
+changeSizeBtn.addEventListener('click', createNewGrid)
+
+createGrid(16);
